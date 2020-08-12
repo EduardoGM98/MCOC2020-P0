d6d37b999e318cd9b6135d968ae542379839b134
EduardoGM98 <69161434+EduardoGM98@users.noreply.github.com>
2020-08-12 19:24:01 -0400

    Add files via upload

---
+++ timing_inv_caso_3_half.py
@@ -0,0 +1,34 @@
+from scipy import matrix, rand, linalg, half
+from time import perf_counter
+from matplotlib import pyplot 
+import numpy as np
+
+lista_N=[3,4, 10, 12, 17, 20, 30, 43, 47, 50, 55, 60, 80, 100, 140, 180, 200, 250, 350, 500]
+lista_T=[]
+mem=[]
+
+def matriz_laplaciana(N, dtype=half): 
+    A=np.zeros((N,N), dtype=dtype)
+    for i in range(N):
+        for j in range(N):
+            if i==j:
+                A[i,j]=2
+            if i+1==j:
+                A[i,j]=-1
+            if i-1==j:
+                A[i,j]=-1
+    return A
+
+for i in lista_N:
+    A=matriz_laplaciana(i)
+    t1 = perf_counter()
+    A1=linalg.inv(A, overwrite_a=True)
+    t2 = perf_counter()
+    dt = t2 - t1
+    lista_T.append(dt)
+    size=2*(i**2)*16 
+    mem.append(size)
+
+print(lista_T)
+print(mem)
+
